2f5cadedd94c2beb8db08006a62dbd5a8cf8ed2b
ll98abc <ll987abc@gmail.com>
2016-09-10 20:23:19 +0800

    update for enemy name

--- illya/js/freeQuest.js
+++ illya/js/freeQuest.js
@@ -7,7 +7,7 @@
 			name: "freeQuest.1 お菓子の国 rank D",
 			cost: "15",
 			prev: "對魔法☆少女童謠醬造成35k傷害 (擊倒1次)",
-			enemy: "狂薑餅人(7k)*3/狂薑餅人(8k)*2+德翁(19k)",
+			enemy: "狂薑餅人(7k)*3/狂薑餅人(8k)*2+不是♂少女德翁子(19k)",
 			drop: "布丁",
 			locate: "a_4" ,
 			tag: "biscuitGolem/saber/neutral/normal"
@@ -17,7 +17,7 @@
 			name: "freeQuest.2 お菓子の国 rank C",
 			cost: "15",
 			prev: "對魔法☆少女童謠醬造成100k傷害 (擊倒3次)",
-			enemy: "弓骨兵(5k)*3/弓骨兵(8k)*2+阿塔喵喵(26k)",
+			enemy: "弓骨兵(5k)*3/弓骨兵(8k)*2+貓耳☆少女阿塔喵(26k)",
 			drop: "DVD",
 			locate: "a_2",
 			tag: "skeleton/archer/neutral/evil"
@@ -28,7 +28,7 @@
 			name: "freeQuest.3 お菓子の国 rank B",
 			cost: "15",
 			prev: "對魔法☆少女童謠醬造成100k傷害 (擊倒3次)",
-			enemy: "殺人馬(5k)*3/殺人馬(8k)*2+嫁怪芙蘭(25k)",
+			enemy: "殺人馬(5k)*3/殺人馬(8k)*2+婚紗☆少女芙蘭肯(25k)",
 			drop: "獅子君",
 			locate: "a_4",
 			tag: "horseman/berserker/chaos/normal"
@@ -38,7 +38,7 @@
 			name: "freeQuest.4 お菓子の国 rank A",
 			cost: "20",
 			prev: "mission.3", 
-			enemy: "槍人馬(6k)*2+狂薑餅人(8k)/槍人馬(13k)*2+狂薑餅人(13k)/奇美拉(22k)+黑槍傻(26k)",
+			enemy: "槍人馬(6k)*2+狂薑餅人(8k)/槍人馬(13k)*2+狂薑餅人(13k)/奇美拉(22k)+爆乳☆少女黑槍傻(26k)",
 			drop: "布丁+DVD+獅子君",
 			locate: "a_1",
 			tag: "biscuitGolem/horseman/lancer/lawful/evil/dragon"
@@ -48,7 +48,7 @@
 			name: "freeQuest.5 大海原と竜の国 rank D",
 			cost: "20",
 			prev: "對治癒☆少女C子蘿造成200k傷害(擊倒2次)",
-			enemy: "弓龍牙兵(8k)*2+殺幽靈(8k)/弓龍牙兵(9k)*2+殺幽靈(9k)/殺幽靈(11k)+瑪塔‧哈里(53k)",
+			enemy: "弓龍牙兵(8k)*2+殺幽靈(8k)/弓龍牙兵(9k)*2+殺幽靈(9k)/殺幽靈(11k)+間諜☆少女瑪塔子(53k)",
 			drop: "布丁+DVD",
 			locate: "b_3",
 			tag: "skeleton/ghost/assassin/chaos/normal"
@@ -58,7 +58,7 @@
 			name: "freeQuest.6 大海原と竜の国 rank C",
 			cost: "20",
 			prev: "對治癒☆少女C子蘿造成200k傷害(擊倒2次)",
-			enemy: "槍飛龍(8k)+殺龍牙兵(8k)/槍飛龍(10k)*2+殺龍牙兵(10k)/槍飛龍(15k)*2+槍龍娘(43k)",
+			enemy: "槍飛龍(8k)+殺龍牙兵(8k)/槍飛龍(10k)*2+殺龍牙兵(10k)/槍飛龍(15k)*2+偶像☆少女槍龍娘(43k)",
 			drop: "布丁+DVD+獅子君",
 			locate: "b_1",
 			tag: "rainbowWyvern/skeleton/lancer/chaos/evil/dragon"
@@ -68,7 +68,7 @@
 			name: "freeQuest.7 大海原と竜の国 rank B",
 			cost: "20",
 			prev: "對治癒☆少女C子蘿造成200k傷害(擊倒2次)",
-			enemy: "殺幽靈(9k)*2+槍飛龍(9k)/殺幽靈(10k)*2+殺幽靈(11k)/槍飛龍(15k)*2+美杜莎(43k)",
+			enemy: "殺幽靈(9k)*2+槍飛龍(9k)/殺幽靈(10k)*2+殺幽靈(11k)/槍飛龍(15k)*2+魔法☆御姊美杜莎(43k)",
 			drop: "DVD+獅子君",
 			locate: "b_2",
 			tag: "ghost/rainbowWyvern/rider/chaos/good/god"
@@ -78,7 +78,7 @@
 			name: "freeQuest.8 大海原と竜の国 rank A",
 			cost: "20",
 			prev: "對治癒☆少女C子蘿造成500k傷害(擊倒4次)",
-			enemy: "劍龍牙兵(8k)*3/殺巨龍(53k)/劍龍牙兵(12k)+藍傻(50k)",
+			enemy: "劍龍牙兵(8k)*3/殺巨龍(53k)/劍龍牙兵(12k)+大食☆少女藍傻巴(50k)",
 			drop: "布丁+DVD+獅子君",
 			locate: "b_1",
 			tag: "skeleton/saber/lawful/good/dragon"
@@ -88,7 +88,7 @@
 			name: "freeQuest.9 死せる書架の国 rank D",
 			cost: "20",
 			prev: "unknown",
-			enemy: "殺女鬼(?)*3/殺女鬼(?)*3/靜謐(?)+殺女鬼(?)*2",
+			enemy: "殺女鬼(?)*3/殺女鬼(?)*3/殺女鬼(?)*2+猛毒☆少女靜謐炭(?)",
 			drop: "DVD",
 			locate: "c_1",
 			tag: "ghost/assassin/lawful/evil"
