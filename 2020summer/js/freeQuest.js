1a938bdc19bf11aa00653c6bf5dc6820377e55da
ll98abc <ll987abc@gmail.com>
2020-08-28 21:25:34 +0800

    update tags

--- 2020summer/js/freeQuest.js
+++ 2020summer/js/freeQuest.js
@@ -24,7 +24,11 @@
 	 * var lancer  = "槍兵";
 	 * var caster  = "術職";
 	 * var assassin= "殺職";  
-	 * var berserker ="狂戰士";   
+	 * var berserker ="狂戰士"; 
+	 * var inside  = "屋內";
+	 * var outside = "屋外";
+	 * var day     = "日間";
+	 * var night   = "夜間";
 	 */
 	
 	freeQuestList[0] = {			
@@ -34,7 +38,7 @@
 		enemy: "狼*3+魔豬*5+狼(稀有)",
 		drop: "乾柴(多)/野菜(少)/種子",
 		locate: "cottage_A_day" ,
-		tag: "dangerPig/beast/demonic/lancer/hog"
+		tag: "dangerPig/beast/demonic/lancer/hog/day/outside"
 	}	
 	
 	freeQuestList[1] = {			
@@ -44,7 +48,7 @@
 		enemy: "大鬼*1+小鬼*7+小鬼(稀有)",
 		drop: "乾柴(多)/野菜(少)/雙晶",
 		locate: "cottage_A_night" ,
-		tag: "demonic/huge/lancer/hog"
+		tag: "demonic/huge/lancer/hog/night/outside"
 	}
 	
 	freeQuestList[2] = {			
@@ -54,7 +58,7 @@
 		enemy: "寄居蟹*8+寄居蟹(稀有)",
 		drop: "野菜(多)/鏈鋸(少)/貝殼",
 		locate: "lakeside_A_day" ,
-		tag: "crab/dangerCrab/demonic/saber"
+		tag: "crab/dangerCrab/demonic/saber/day/outside"
 	}
 	
 	freeQuestList[3] = {			
@@ -64,7 +68,7 @@
 		enemy: "海魔*8+海魔(稀有)",
 		drop: "野菜(多)/鏈鋸(少)/蛇眼",
 		locate: "lakeside_A_night" ,
-		tag: "demonic/saber"
+		tag: "demonic/saber/night/outside"
 	}		
 		
 	freeQuestList[4] = {			
@@ -74,7 +78,7 @@
 		enemy: "巨龍+飛龍*7+飛龍(稀有)",
 		drop: "鏈鋸(多)/乾柴(少)/龍鱗",
 		locate: "deepForest_A_day" ,
-		tag: "dragon/huge/berserker/wyvern"				
+		tag: "dragon/huge/berserker/wyvern/day/outside"
 	}
 	
 	freeQuestList[5] = {			
@@ -84,7 +88,7 @@
 		enemy: "大蛇+骷髏兵*7+骷髏兵(稀有)",
 		drop: "鏈鋸(多)/乾柴(少)/神酒/大騎士勳章",
 		locate: "deepForest_A_night" ,
-		tag: "beast/necro/berserker/huge/demonic"				
+		tag: "beast/necro/berserker/huge/demonic/night/outside"
 	}
 	
 	freeQuestList[6] = {			
@@ -94,7 +98,7 @@
 		enemy: "奇美拉+蟲群*7+蟲群(稀有)",
 		drop: "膽石/髓液/鏈鋸(多)/乾柴(少)",
 		locate: "westBuilding_A_day" ,
-		tag: "bugs/archer/beast"				
+		tag: "bugs/archer/beast/day/inside"	
 	}
 	
 	freeQuestList[7] = {			
@@ -104,7 +108,7 @@
 		enemy: "大惡魔/人偶*7+人偶(稀有)",
 		drop: "心臟/齒輪/鏈鋸(多)/乾柴(少)",
 		locate: "westBuilding_A_night" ,
-		tag: "demonic/archer"				
+		tag: "demonic/archer/night/inside"
 	}
 	
 	freeQuestList[8] = {			
@@ -114,7 +118,7 @@
 		enemy: "雙角馬+魔書*7+魔書(稀有)",
 		drop: "角角/書頁/三素材混搭",
 		locate: "mansion_A_day" ,
-		tag: "hornHorse/beast/demonic/archer/book"				
+		tag: "hornHorse/beast/demonic/archer/book/day/inside"
 	}
 	
 	freeQuestList[9] = {			
@@ -124,7 +128,7 @@
 		enemy: "眼球+幽靈*7+幽靈(稀有)",
 		drop: "鬼燈/血淚/三素材混搭",
 		locate: "mansion_A_night" ,
-		tag: "demonic/necro/archer"				
+		tag: "demonic/necro/archer/night/inside"
 	}
 	
 	freeQuestList[10] = {			
@@ -134,7 +138,7 @@
 			enemy: "白色狗狗+蛇蜥*7+蛇蜥(稀有)",
 			drop: "臭臭泥/龍牙/野菜(多)/鏈鋸(少)",
 			locate: "market_A_day" ,
-			tag: "soulEater/beast/demonic/god/caster/basilisk"				
+			tag: "soulEater/beast/demonic/god/caster/basilisk/day/inside"
 	}
 	
 	freeQuestList[11] = {			
@@ -144,7 +148,7 @@
 			enemy: "白鳥+喪屍*7+喪屍",
 			drop: "羽毛/鐵釘/野菜(多)/鏈鋸(少)",
 			locate: "market_A_night" ,
-			tag: "necro/beast/demonic/god/caster/human"
+			tag: "necro/beast/demonic/god/caster/human/night/inside"
 	}
 	
 	freeQuestList[12] = {			
@@ -154,7 +158,7 @@
 			enemy: "斯芬克斯+雞*7+雞(稀有)",
 			drop: "神燈/箭頭/乾柴(多)/野菜(少)",
 			locate: "house_A_day" ,
-			tag: "sphinx/huge/god/chicken/lancer"				
+			tag: "sphinx/huge/god/chicken/lancer/day/outside"
 	}
 	
 	freeQuestList[13] = {			
@@ -164,7 +168,7 @@
 			enemy: "大幽靈+7女鬼+女鬼(稀有)",
 			drop: "鏡子/黑粉/乾柴(多)/野菜(少)",
 			locate: "house_A_night" ,
-			tag: "necro/demonic/huge/human/lancer"
+			tag: "necro/demonic/huge/human/lancer/night/outside"
 	}
 	
 	freeQuestList[14] = {			
@@ -174,7 +178,7 @@
 			enemy: "呂布+巨石像+猴子*2+猴子(稀有)，1、1、(2+1)配置",
 			drop: "三素材混搭/蕃薯藤/頭冠",
 			locate: "smilingHotel_day" ,
-			tag: "berserker/spriggan/beast/huge/demonic"
+			tag: "berserker/spriggan/beast/huge/demonic/day/inside"
 	}
 	freeQuestList[15] = {			
 			name: "微笑旅社_night",
@@ -183,7 +187,7 @@
 			enemy: "刺繡公/紅毛獅子/殺人鬼*2+殺人鬼(稀有)，1、1、(2+1)配置",
 			drop: "三素材混搭/獅子毛/鎖鍊",
 			locate: "smilingHotel_night" ,
-			tag: "human/berserker/beast/demonic"
+			tag: "human/berserker/beast/demonic/night/outside"
 	}
 	freeQuestList[16] = {			
 			name: "小屋B_day",
@@ -192,7 +196,7 @@
 			enemy: "狼*3+魔豬*5+狼(稀有)",
 			drop: "乾柴(多)/野菜(少)/種子",
 			locate: "cottage_B_day" ,
-			tag: "dangerPig/beast/demonic/lancer/hog"
+			tag: "dangerPig/beast/demonic/lancer/hog/day/outside"
 	}
 	
 	freeQuestList[17] = {			
@@ -203,7 +207,7 @@
 			enemy: "大鬼*1+小鬼*7+小鬼(稀有)",
 			drop: "乾柴(多)/野菜(少)/雙晶",
 			locate: "cottage_B_night" ,
-			tag: "necro/demonic/huge/lancer"				
+			tag: "necro/demonic/huge/lancer/night/outside"		
 	}
 	freeQuestList[18] = {			
 			name: "湖畔B_day",
@@ -212,7 +216,7 @@
 			enemy: "寄居蟹*8+寄居蟹(稀有)",
 			drop: "野菜(多)/鏈鋸(少)/貝殼",
 			locate: "lakeside_B_day" ,
-			tag: "crab/dangerCrab/demonic/saber"
+			tag: "crab/dangerCrab/demonic/saber/day/outside"
 	}
 	
 	freeQuestList[19] = {			
@@ -222,7 +226,7 @@
 			enemy: "海魔*8+海魔(稀有)",
 			drop: "野菜(多)/鏈鋸(少)/蛇眼",
 			locate: "lakeside_B_night" ,
-			tag: "demonic/saber"				
+			tag: "demonic/saber/night/outside"	
 	}
 	
 	freeQuestList[20] = {			
@@ -232,7 +236,7 @@
 			enemy: "巨龍+飛龍*7+飛龍(稀有)",
 			drop: "鏈鋸(多)/乾柴(少)/龍鱗/",
 			locate: "deepForest_B_day" ,
-			tag: "dragon/huge/berserker/wyvern"
+			tag: "dragon/huge/berserker/wyvern/day/outside"
 	}
 	
 	freeQuestList[21] = {			
@@ -242,7 +246,7 @@
 			enemy: "大蛇+骷髏兵*7+骷髏兵(稀有)",
 			drop: "鏈鋸(多)/乾柴(少)/神酒/大騎士勳章",
 			locate: "deepForest_B_night" ,
-			tag: "beast/necro/berserker/huge/demonic"
+			tag: "beast/necro/berserker/huge/demonic/night/outside"
 	}
 	
 	freeQuestList[22] = {			
@@ -252,7 +256,7 @@
 			enemy: "娜迦+蟲群*7+蟲群(稀有)",
 			drop: "金蛋/髓液/鏈鋸(多)/乾柴(少)",
 			locate: "westBuilding_B_day" ,
-			tag: "dragon/archer/bugs/god/huge/demonic"				
+			tag: "dragon/archer/bugs/god/huge/demonic/day/inside"			
 	}
 		
 	freeQuestList[23] = {			
@@ -262,7 +266,7 @@
 			enemy: "虎戰車/人偶*7+人偶(稀有)",
 			drop: "爐心/齒輪/鏈鋸(多)/乾柴(少)",
 			locate: "westBuilding_B_night" ,
-			tag: "archer"				
+			tag: "archer/night/inside"
 	}
 	
 	freeQuestList[24] = {			
@@ -272,7 +276,7 @@
 			enemy: "雙角馬+魔書*7+魔書(稀有)",
 			drop: "角角/書頁/三素材混搭",
 			locate: "mansion_B_day" ,
-			tag: "hornHorse/beast/demonic/archer/book"				
+			tag: "hornHorse/beast/demonic/archer/book/day/inside"
 	}
 		
 	freeQuestList[25] = {			
@@ -282,7 +286,7 @@
 			enemy: "眼球+幽靈*7+幽靈(稀有)",
 			drop: "鬼燈/血淚/三素材混搭",
 			locate: "mansion_B_night" ,
-			tag: "demonic/necro/archer"				
+			tag: "demonic/necro/archer/night/inside"	
 	}
 	
 	freeQuestList[26] = {			
@@ -292,7 +296,7 @@
 			enemy: "黑色狗狗+蛇蜥*7+蛇蜥(稀有)",
 			drop: "臭臭泥/龍牙/野菜(多)/鏈鋸(少)",
 			locate: "market_B_day" ,
-			tag: "soulEater/beast/demonic/god/caster/basilisk"				
+			tag: "soulEater/beast/demonic/god/caster/basilisk/day/inside"
 	}
 	
 	freeQuestList[27] = {			
@@ -302,7 +306,7 @@
 			enemy: "黑鳥+喪屍*7+喪屍",
 			drop: "羽毛/鐵釘/野菜(多)/鏈鋸(少)",
 			locate: "market_B_night" ,
-			tag: "necro/beast/demonic/caster/human"
+			tag: "necro/beast/demonic/caster/human/night/inside"
 	}
 	
 	freeQuestList[28] = {			
@@ -312,7 +316,7 @@
 			enemy: "斯芬克斯+雞*7+雞(稀有)",
 			drop: "神燈/箭頭/乾柴(多)/野菜(少)",
 			locate: "house_B_day" ,
-			tag: "sphinx/huge/god/chicken/lancer"				
+			tag: "sphinx/huge/god/chicken/lancer/day/outside"		
 	}
 	
 	freeQuestList[29] = {			
@@ -322,6 +326,6 @@
 			enemy: "大幽靈+7女鬼+女鬼(稀有)",
 			drop: "鏡子/黑粉/乾柴(多)/野菜(少)",
 			locate: "house_B_night" ,
-			tag: "necro/demonic/huge/human/lancer"
+			tag: "necro/demonic/huge/human/lancer/night/outside"
 	}
 }
